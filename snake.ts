74a498feac71c1a649b3938b5cfe151261efe005
brett <brett.labash@gmail.com>
2019-10-18 17:16:17 -0700

    Add snake body that follows head

--- snake.ts
+++ snake.ts
@@ -1,6 +1,7 @@
 import { board } from './boardSettings';
 
 type MoveDir = 'up' | 'right' | 'down' | 'left';
+type SnakeNode = [number, number];
 
 export class Snake {
   private ctx: CanvasRenderingContext2D;
@@ -11,6 +12,9 @@ export class Snake {
   private updateFrame: number = 0;
   private direction: string = 'right';
   private moveQueue: MoveDir[] = [];
+  private tail: SnakeNode;
+  private newHead: SnakeNode;
+  snakeBody: SnakeNode[] = [[5, 5], [4, 5], [3, 5], [2, 5]];
 
   constructor(private canvas: HTMLCanvasElement) {
     this.ctx = canvas.getContext('2d');
@@ -19,8 +23,8 @@ export class Snake {
     this.cellHeight = canvas.height / board.dimX;
     // this.direction = 'right';
 
-    this.x = 0;
-    this.y = 0;
+    this.x = this.snakeBody[0][0];
+    this.y = this.snakeBody[0][1];
     window.addEventListener('keydown', this.changeDirection.bind(this));
   }
 
@@ -61,12 +65,20 @@ export class Snake {
 
   public draw() {
     this.ctx.fillStyle = '#ff0000';
-    this.ctx.fillRect(
-      this.x * this.cellWidth,
-      this.y * this.cellHeight,
-      30,
-      30
-    );
+    for (let i = 0; i < this.snakeBody.length; i++) {
+      let node = this.snakeBody[i];
+      this.ctx.fillRect(
+        node[0] * this.cellWidth,
+        node[1] * this.cellHeight,
+        30,
+        30
+      );
+    }
+  }
+
+  public updateSnakeBody(head: SnakeNode) {
+    this.snakeBody.unshift(head);
+    this.snakeBody.pop();
   }
 
   public update() {
@@ -93,6 +105,8 @@ export class Snake {
       if (this.x < 0) this.x = board.dimX;
       if (this.y === board.dimY) this.y = 0;
       if (this.y < 0) this.y = board.dimY;
+
+      this.updateSnakeBody([this.x, this.y]);
     }
   }
 }
